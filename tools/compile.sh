98cb3fd7b78eb76fb54206ced19d0a3096eafcfe
Pierre Benard <pierre.g.benard@inria.fr>
2019-05-14 13:41:16 +0200

    Add travis automatic build

---
+++ tools/compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+sh -f configure.sh Emscripten-clang
+cd build/Emscripten-clang-Release
+cmake --build .
+cd bin
+cp ../../../data/pig.obj . 
+sh ../../../tools/gen_emscripten_html.sh contours_viewer.js pig.obj
