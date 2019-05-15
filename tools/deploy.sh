f83d772c0cc5ae50f35386e50d00fd9c9cd6dcd4
Pierre Benard <pierre.g.benard@inria.fr>
2019-05-15 07:40:35 +0200

    Deploy

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -53,7 +53,12 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+DIRECTORY=~/.ssh
+if [ ! -d "$DIRECTORY" ]; then
+  mkdir "$DIRECTORY"
+fi
+ssh-keyscan github.com >> "$DIRECTORY"/known_hosts
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../tools/travis_rsa.enc -out ../deploy_key -d
 chmod 600 ../deploy_key
 eval `ssh-agent -s`
