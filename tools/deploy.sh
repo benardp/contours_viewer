64ca9c86d4dfbfd8ba533d1f4896ccb30649b660
Pierre Benard <pierre.g.benard@inria.fr>
2019-05-15 08:30:21 +0200

    Fix permission

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -46,11 +46,11 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 
-DIRECTORY=~/.ssh
-if [ ! -d "$DIRECTORY" ]; then
-  mkdir "$DIRECTORY"
-fi
-ssh-keyscan github.com >> "$DIRECTORY"/known_hosts
+# DIRECTORY=~/.ssh
+# if [ ! -d "$DIRECTORY" ]; then
+#   mkdir "$DIRECTORY"
+# fi
+# ssh-keyscan github.com >> "$DIRECTORY"/known_hosts
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../tools/travis_rsa.enc -out ../deploy_key -d
 chmod 600 ../deploy_key
 eval `ssh-agent -s`
